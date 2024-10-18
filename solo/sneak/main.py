be7216837df8253c44ec5d9e50e8689e9ccecb08
tyusei04 <yruisnegio2420@icloud.com>
2024-10-18 15:28:05 +0900

    test1

--- solo/sneak/main.py
+++ solo/sneak/main.py
@@ -1,5 +1,4 @@
-# This file can be a nice home for your Battlesnake logic and helper functions.
-#
+
 # To get you started we've included code to prevent your Battlesnake from moving backwards.
 # For more info see docs.battlesnake.com test
 import random
