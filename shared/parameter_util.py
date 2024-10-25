99b8e0d3f24a1609de5099cae6b4993c6eb424b6
kuro984 <u911051b@ecs.osaka-u.ac.jp>
2024-10-25 09:11:36 +0900

    4方向について取得(仮)

--- shared/parameter_util.py
+++ shared/parameter_util.py
@@ -2,3 +2,55 @@
 # example:
 #  def get_snake_length(game_state: dict) -> int:
 #      return game_state["you"]["length"]
+
+def get_up_body(game_state: dict) -> int:
+    r =0
+    head = game_state["you"]["head"]
+    body = ["data"]["body"]
+    check_point1 = {"x": head["x"], "y": head["y"] - 1}
+    check_point2 = {"x": head["x"], "y": head["y"] - 2}
+
+    if check_point1 in body:
+        r += 1
+    if check_point2 in body:
+        r += 2
+    return r
+
+def get_down_body(game_state: dict) -> int:
+    r =0
+    head = game_state["you"]["head"]
+    body = ["data"]["body"]
+    check_point1 = {"x": head["x"], "y": head["y"] + 1}
+    check_point2 = {"x": head["x"], "y": head["y"] + 2}
+
+    if check_point1 in body:
+        r += 1
+    if check_point2 in body:
+        r += 2
+    return r
+
+def get_right_body(game_state: dict) -> int:
+    r =0
+    head = game_state["you"]["head"]
+    body = ["data"]["body"]
+    check_point1 = {"x": head["x"] + 1 , "y": head["y"]}
+    check_point2 = {"x": head["x"] + 2 , "y": head["y"]}
+
+    if check_point1 in body:
+        r += 1
+    if check_point2 in body:
+        r += 2
+    return r
+
+def get_left_body(game_state: dict) -> int:
+    r =0
+    head = game_state["you"]["head"]
+    body = ["data"]["body"]
+    check_point1 = {"x": head["x"] - 1 , "y": head["y"]}
+    check_point2 = {"x": head["x"] - 2 , "y": head["y"]}
+
+    if check_point1 in body:
+        r += 1
+    if check_point2 in body:
+        r += 2
+    return r
