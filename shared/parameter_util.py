1616d0d3d28b11e6aa74b65ccf45708351f1cf05
tkhs <tokuhausu@gmail.com>
2024-10-25 15:43:08 +0900

    消えていた分を復旧

--- shared/parameter_util.py
+++ shared/parameter_util.py
@@ -122,3 +122,38 @@ def get_leftd_body(game_state: dict) -> int:
     if check_point2 in body:
         r += 1
     return r
+
+def get_snake_foods(game_state: dict) -> int:
+    food_max=0
+    my_head=game_state["you"]["head"]
+
+    for i in range(3):
+        feed=game_state["board"]["food"][i]
+        food=abs(my_head["x"]-feed["x"])+abs(my_head["y"]-feed["y"])
+        if food >= food_max:
+            food_max=food
+
+    return food
+
+#壁との距離
+def get_snake_distance(game_state: dict) -> int:
+    coordinate_x = game_state["you"]["head"]["x"]
+    coordinate_y = game_state["you"]["head"]["y"]
+    x_distance = 7 - coordinate_x
+    y_distance = 7 - coordinate_y
+    min_distance = min(coordinate_x, coordinate_y, x_distance, y_distance)
+    return min_distance
+
+
+
+#蛇の長さ
+def get_snake_length(game_state: dict) -> int:
+    length = game_state["you"]["length"]
+
+    return length
+
+#ヘルス
+def get_snake_health(game_state: dict) -> int:
+    health = game_state["you"]["health"]
+
+    return health
