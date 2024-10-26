f0aa315ae9e836862d4228bfd3d892654a3d92ad
tkhs <tokuhausu@gmail.com>
2024-10-26 23:08:56 +0900

    battlesnakeのゲームを処理するgoのプログラムとそのdllを追加

---
+++ solo/trainer/rules.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+import ctypes
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+lib = ctypes.CDLL("../../embedded-rules/build/rules.dll")
+CALLBACKFUNC = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_char_p)
+
+class Client(ctypes.Structure):
+    _fields_ = [
+        ("on_start", CALLBACKFUNC),
+        ("on_move", CALLBACKFUNC),
+        ("on_end", CALLBACKFUNC)
+    ]
+
+class GameSettings(ctypes.Structure):
+    _fields_ = [
+        ("seed", ctypes.c_long),
+        ("width", ctypes.c_int),
+        ("height", ctypes.c_int),
+        ("food_spawn_chance", ctypes.c_int),
+        ("minimum_food", ctypes.c_int)
+    ]
+
+def start_solo_game(callback_funcs : Client, settings : GameSettings):
+    lib.startSoloGame(callback_funcs, settings)
