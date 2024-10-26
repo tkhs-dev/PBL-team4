636a6c8645d7ad52a17e9a292b3ea49519c03dc2
tkhs <tokuhausu@gmail.com>
2024-10-26 16:21:35 +0900

    遺伝的学習実装途中

--- solo/trainer/main.py
+++ solo/trainer/main.py
@@ -1,2 +1,9 @@
+from deap import creator, base
+
+
+def train():
+    creator.create("FitnessMin", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMin)
 
 if __name__ == "__main__":
+    train()
