b681eb169ed54d2deee9d94febac0096bd09735d
tkhs <tokuhausu@gmail.com>
2024-11-15 16:58:53 +0900

    修正

--- solo/run_solo.bat
+++ solo/run_solo.bat
@@ -1,6 +1,6 @@
 cd /d %~dp0
 pip3 install -r ../requirements.txt
 python ../setup.py
-start python ./sneak/main.py ../evaluator.pth
+start python ./sneak/main.py ./evaluator.pth
 timeout 4
 ..\rules\battlesnake.exe play -W 6 -H 6 --name 'Local Solo Test' --url http://localhost:8000 -g solo --browser --minimumFood 3 --foodSpawnChance 0
