a2419609f714104695e6a6c9e6ff3b7ebc71ab49
tkhs <tokuhausu@gmail.com>
2024-11-30 12:10:01 +0900

    空白マスのチャンネルを追加

--- re_solo/sneak/evaluator.py
+++ re_solo/sneak/evaluator.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"
 from shared.parameter_util import get_front_body, get_left_body, get_right_body, get_leftd_body, get_rightd_body, \
     get_snake_length, get_snake_health, get_snake_distance, get_snake_foods, get_free_space
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Evaluator:
     def __init__(self):
@@ -23,22 +24,26 @@ class Evaluator:
         return evaluator
 
     def evaluate(self, game_state):
-        return self.model(*map(lambda x:x.unsqueeze(0),self.get_input_tensor(game_state))).squeeze().tolist()
+        return self.model(*map(lambda x:x.unsqueeze(0).to(device),self.get_input_tensor(game_state))).squeeze().tolist()
 
     @staticmethod
     def get_input_tensor(game_state : dict) -> (torch.Tensor, torch.Tensor):
         body_board = np.zeros((6,6), dtype=np.integer)
         head_board = np.zeros((6,6), dtype=np.integer)
         food_board = np.zeros((6,6), dtype=np.integer)
+        free_board = np.ones((6,6), dtype=np.integer)
         you = game_state["you"]
         for body in you["body"]:
             if 0 <= body["x"] < 6 and 0 <= body["y"] < 6:
                 body_board[body["y"]][body["x"]] = 1
+                free_board[body["y"]][body["x"]] = 0
         if 0 <= you["head"]["x"] < 6 and 0 <= you["head"]["y"] < 6:
             head_board[you["head"]["y"]][you["head"]["x"]] = 1
+            free_board[you["head"]["y"]][you["head"]["x"]] = 0
         for food in game_state["board"]["food"]:
             food_board[food["y"]][food["x"]] = 1
-        one_hot_encoded = np.stack([body_board, head_board, food_board], axis=0)
+            free_board[food["y"]][food["x"]] = 0
+        one_hot_encoded = np.stack([body_board, head_board, food_board, free_board], axis=0)
         board_tensor = torch.tensor(one_hot_encoded, dtype=torch.float32)
         game_tensor = torch.tensor([get_snake_health(game_state)/100,get_snake_length(game_state)/36], dtype=torch.float32)
         return board_tensor,game_tensor
@@ -48,7 +53,7 @@ class EvaluatorModel(nn.Module):
     def __init__(self):
         super(EvaluatorModel, self).__init__()
         self.cnn = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(4, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
@@ -69,8 +74,10 @@ class EvaluatorModel(nn.Module):
 
     def load(self, path):
         try:
+            self.cuda(device)
             self.load_state_dict(torch.load(path, weights_only=True))
-        except:
+        except Exception as e:
+            print(e)
             self.apply(self._initialize_with_random_weights)
             print("Failed to load model. Randomly initialized")
         return
