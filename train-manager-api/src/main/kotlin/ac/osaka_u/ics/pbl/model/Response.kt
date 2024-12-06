219ce236ab8bcaef881d699c44da579aaa421d1f
hougelin <kmkhgl2002@126.com>
2024-12-06 16:23:11 +0900

    define data class(response) #12

---
+++ train-manager-api/src/main/kotlin/ac/osaka_u/ics/pbl/model/Response.kt
@@ -0,0 +1,23 @@
+package ac.osaka_u.ics.pbl.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NextResponse(
+    val id: String,
+    val assigned_at: String,
+    val client: String,
+    val deadline: Long,
+    val status: String,
+    val ststus_changed_at: Long,
+    val task: Task
+)
+
+data class RefreshResponse(
+    val id: String,
+    val deadline: Long
+)
+
+
+
+
