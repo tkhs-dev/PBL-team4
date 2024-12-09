e3d6f571681ae860d7eff6a238348b5e5b663af7
tkhs <tokuhausu@gmail.com>
2024-12-09 23:03:06 +0900

    debugオプション及びデバッグ用の認証トークンを追加

--- train-manager-api/src/main/kotlin/ac/osaka_u/ics/pbl/Application.kt
+++ train-manager-api/src/main/kotlin/ac/osaka_u/ics/pbl/Application.kt
@@ -1,24 +1,21 @@
 package ac.osaka_u.ics.pbl
 
+import ac.osaka_u.ics.pbl.domain.model.Client
 import ac.osaka_u.ics.pbl.domain.repos.*
 import ac.osaka_u.ics.pbl.handler.TasksHandler
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.resources.*
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.koin.ktor.ext.get
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, port = 8080) {
-        module()
-    }.start(wait = true)
-}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     configureDI()
@@ -30,6 +27,10 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+    onDebug {
+        val clientRepo = get<ClientRepository>()
+        clientRepo.createClient(Client(-1, "test"))
+    }
 }
 
 fun Application.configureDI() {
@@ -39,9 +40,16 @@ fun Application.configureDI() {
     }
 }
 
+fun Application.onDebug(block: Application.() -> Unit) {
+    if (debug) block()
+}
+
 val module = module {
     singleOf(::TaskRepositoryImpl){bind<TaskRepository>()}
     singleOf(::TaskGeneratorRepositoryImpl){bind<TaskGeneratorRepository>()}
     singleOf(::ClientRepositoryImpl){bind<ClientRepository>()}
     singleOf(::TasksHandler)
 }
+
+val Application.debug: Boolean
+    get() = environment.config.propertyOrNull("ktor.deployment.debug")?.getString()?.toBoolean() ?: false
