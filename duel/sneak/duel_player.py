9aa35547bf454ed1a4f4c16cce3155d0e6c22724
tkhs <tokuhausu@gmail.com>
2025-01-03 16:33:01 +0900

    安全な行動のみを選択できるように変更

--- duel/sneak/duel_player.py
+++ duel/sneak/duel_player.py
@@ -2,22 +2,29 @@ import os
 import sys
 
 from duel.sneak.duel_evaluator import Evaluator
-from shared.rule import Direction
+from shared.rule import Direction, move, TurnResult
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from shared.player import IPlayer
 
 class AIPlayer(IPlayer):
-    def __init__(self, evaluator:Evaluator):
+    def __init__(self, evaluator:Evaluator, safe:bool = False):
         self.evaluator = evaluator
+        self.safe = safe
 
     def on_start(self, game_state):
-        print("GAME START")
+        return 0
 
     def on_end(self, game_state):
-        print("GAME OVER\n")
+        return 0
 
     def on_move(self, game_state):
         q = self.evaluator.evaluate(game_state)
-        direction = max(zip(Direction,q), key = lambda x:x[1])[0]
+        if not self.safe:
+            direction = max(zip(Direction,q), key = lambda x:x[1])[0]
+            return direction
+        safes = list(filter(lambda x:move(game_state,x[0])[0]!=TurnResult.LOSE, zip(Direction,q)))
+        if len(safes) == 0:
+            return Direction.UP
+        direction = max(safes, key = lambda x:x[1])[0]
         return direction
