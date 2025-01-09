231d9ebc1eb0b6222177de4a689381b2cb69abba
tkhs <tokuhausu@gmail.com>
2025-01-09 23:25:16 +0900

    シミュレート時の次ターンのヘビの しっぽを修正

--- shared/rule.py
+++ shared/rule.py
@@ -48,26 +48,6 @@ def move(game_state:dict, direction:Direction) -> (TurnResult, dict):
 
     next_state["turn"] += 1
 
-    #何らかの衝突があるかどうかを判定し,それに応じて処理、結果を返す
-    if _is_head_out_of_bounds(game_state, next_head):
-        return TurnResult.LOSE, None
-    elif _is_head_colliding_with_self(game_state, next_head):
-        return TurnResult.LOSE, None
-    elif (snake := _is_head_colliding_with_other_snake(game_state, next_head)) is not None:
-        #snakeには衝突したsnakeのdictが入る
-        #もし接触したのが他の蛇の頭の場合,蛇の長さに応じて勝敗が変わる
-        #体に接触した場合はこちらの負け
-        if next_head == snake["head"]:
-              if game_state["you"]["length"] > game_state["snake"]["length"]:
-                      return TurnResult.WIN,None
-              elif game_state["you"]["length"] == game_state["snake"]["length"]:
-                      return TurnResult.DRAW,None
-              elif game_state["you"]["length"] < game_state["snake"]["length"]:
-                      return TurnResult.LOSE,None
-        elif next_head in snake["body"]:
-              return TurnResult.LOSE,None
-
-
     if _is_head_colliding_with_food(game_state, next_head):
         #餌に接触した場合の処理
         next_state["you"]["health"] = 100
@@ -84,13 +64,46 @@ def move(game_state:dict, direction:Direction) -> (TurnResult, dict):
         if next_state["you"]["health"] == 0:
             return TurnResult.LOSE, None
 
+    #他の蛇の情報を更新
+    for snake in next_state["board"]["snakes"]:
+        if snake["id"] == next_state["you"]["id"]:
+            continue
+        if snake["body"][-1] == snake["head"]:
+            snake["body"].pop()
+        else:
+            snake["body"].pop()
+            snake["body"].insert(0,snake["head"])
+        snake["head"] = snake["body"][0]
+        snake["health"] -= 1
+        if snake["health"] == 0:
+            next_state["board"]["snakes"].remove(snake)
+
+    #何らかの衝突があるかどうかを判定し,それに応じて処理、結果を返す
+    if _is_head_out_of_bounds(game_state, next_head):
+        return TurnResult.LOSE, None
+    elif _is_head_colliding_with_self(game_state, next_head):
+        return TurnResult.LOSE, None
+    elif (snake := _is_head_colliding_with_other_snake(game_state, next_head)) is not None:
+        #snakeには衝突したsnakeのdictが入る
+        #もし接触したのが他の蛇の頭の場合,蛇の長さに応じて勝敗が変わる
+        #体に接触した場合はこちらの負け
+        if next_head == snake["head"]:
+            if game_state["you"]["length"] > game_state["snake"]["length"]:
+                return TurnResult.WIN,None
+            elif game_state["you"]["length"] == game_state["snake"]["length"]:
+                return TurnResult.DRAW,None
+            elif game_state["you"]["length"] < game_state["snake"]["length"]:
+                return TurnResult.LOSE,None
+        elif next_head in snake["body"]:
+            return TurnResult.LOSE,None
+
     return TurnResult.CONTINUE, next_state
 
 def _is_head_out_of_bounds(game_state:dict, next_head:(int,int)) -> bool:
     return next_head["x"] < 0 or next_head["x"] >= game_state["board"]["width"] or next_head["y"] < 0 or next_head["y"] >= game_state["board"]["height"]
 
 def _is_head_colliding_with_self(game_state:dict, next_head:(int,int)) -> bool:
-    return next_head in game_state["you"]["body"][:game_state["you"]["length"]]
+    return next_head in game_state["you"]["body"][:game_state["you"]["length"]-1]
 
 def _is_head_colliding_with_other_snake(game_state:dict, next_head:(int,int)) -> dict | None: #return snake dict or None
     for snake in game_state["board"]["snakes"]:
