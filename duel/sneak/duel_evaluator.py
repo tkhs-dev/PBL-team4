afa9952f87354edf9bfeb18e7294163420581155
tkhs <tokuhausu@gmail.com>
2025-01-20 02:02:28 +0900

    ネットワークの構成を更新

--- duel/sneak/duel_evaluator.py
+++ duel/sneak/duel_evaluator.py
@@ -28,8 +28,10 @@ class Evaluator:
     def get_input_tensor(game_state : dict) -> (torch.Tensor, torch.Tensor):
         sneak0_body_board = np.zeros((11,11), dtype=np.int32)
         sneak0_head_board = np.zeros((11,11), dtype=np.int32)
+        sneak0_tail_board = np.zeros((11,11), dtype=np.int32)
         sneak1_body_board = np.zeros((11,11), dtype=np.int32)
         sneak1_head_board = np.zeros((11,11), dtype=np.int32)
+        sneak1_tail_board = np.zeros((11,11), dtype=np.int32)
         food_board = np.zeros((11,11), dtype=np.int32)
         you = game_state["you"]
         for body in you["body"]:
@@ -37,6 +39,9 @@ class Evaluator:
                 sneak0_body_board[body["y"]][body["x"]] = 1
         if 0 <= you["head"]["x"] < 11 and 0 <= you["head"]["y"] < 11:
             sneak0_head_board[you["head"]["y"]][you["head"]["x"]] = 1
+        sneak0_tail = you["body"][-1]
+        if 0 <= sneak0_tail["x"] < 11 and 0 <= sneak0_tail["y"] < 11:
+            sneak0_tail_board[sneak0_tail["y"]][sneak0_tail["x"]] = 1
 
         sneak1_length = 1
         if len(game_state["board"]["snakes"]) == 2:
@@ -47,11 +52,14 @@ class Evaluator:
                     sneak1_body_board[body["y"]][body["x"]] = 1
             if 0 <= sneak1["head"]["x"] < 11 and 0 <= sneak1["head"]["y"] < 11:
                 sneak1_head_board[sneak1["head"]["y"]][sneak1["head"]["x"]] = 1
+            sneak1_tail = sneak1["body"][-1]
+            if 0 <= sneak1_tail["x"] < 11 and 0 <= sneak1_tail["y"] < 11:
+                sneak1_tail_board[sneak1_tail["y"]][sneak1_tail["x"]] = 1
 
         
         for food in game_state["board"]["food"]:
             food_board[food["y"]][food["x"]] = 1
-        one_hot_encoded = np.stack([sneak0_body_board, sneak0_head_board, sneak1_body_board, sneak1_head_board, food_board], axis=0)
+        one_hot_encoded = np.stack([sneak0_body_board, sneak0_head_board, sneak0_tail_board, sneak1_body_board, sneak1_head_board, sneak1_tail_board, food_board], axis=0)
         board_tensor = torch.tensor(one_hot_encoded, dtype=torch.float32)
         game_tensor = torch.tensor([get_snake_health(game_state)/100,get_snake_length(game_state)/121,sneak1_length-get_snake_length(game_state)], dtype=torch.float32)
         return board_tensor,game_tensor
@@ -61,7 +69,7 @@ class EvaluatorModel(nn.Module):
     def __init__(self):
         super(EvaluatorModel, self).__init__()
         self.cnn = nn.Sequential(
-            nn.Conv2d(5, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(7, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
